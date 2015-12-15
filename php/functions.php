c8f1dd08e0034afd8a65f18bd50979d72895e84c
PortierJames <james.maiangowi@utoronto.ca>
2015-12-15 13:20:29 -0500

    minor changes

--- php/functions.php
+++ php/functions.php
@@ -4,16 +4,15 @@ function processLogin($post)
 {
 	$conn = getConnection();
 	
-	$query = "select * from users where user='".$post['username']."' and password=password('".$post['pass']."') LIMIT 1;";
+	$query = "select * from users where user='".$post['username']."' identified by '".$post['pass']."') LIMIT 1;";
 	$results = mysql_query($query);
 	
 	if(mysql_numrows($results)>0)
 	{
 		$row = mysql_fetch_assoc($results);
-		$_SESSION['LoggedIn'] = true;
 		$_SESSION['id'] = $row["id"];
 		$_SESSION['username'] = $row['username'];
-		$_SESSION['LoggedIn'] = true;	
+		$_SESSION['loggedIn'] = true;	
 		return true;
 	}
 	return false;
@@ -22,6 +21,14 @@ function processLogin($post)
 function displayErrorMessage($msg)
 {
 	echo "<div class='err'>{$msg}</div>";
+}
+
+function getConnection()
+{
+	$db = mysql_connect('', '', '');
+	mysql_select_db('');
+	
+	return $conn;
 }
 
 ?>
--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,27 +1,13 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-CREATE TABLE IF NOT EXISTS `links` (
-  `inbound` varchar(128) NOT NULL,
-  `outbound`   varchar(128) NOT NULL,
-  KEY `inbound` (`inbound`, `outbound`),
-  KEY `outbound` (`outbound`)
+CREATE TABLE IF NOT EXISTS `users` (
+  id int(10) primary key,
+  username varchar(128) not null,
+  pass varchar(128) not null
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `urls` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `url` varchar(264) NOT NULL,
-  `title` varchar(128) DEFAULT NULL,
-  `crawled` int(1) NOT NULL DEFAULT '0',
-  `clicks` int(3) DEFAULT NULL,
-  `http_code` int(3) DEFAULT NULL,
-  `size` int(11) DEFAULT NULL,
-  `type` varchar(64) DEFAULT NULL,
-  `modified` int(15) DEFAULT NULL,
-  `md5` varchar(32) DEFAULT NULL,
-  `crawl_tag` varchar(32) DEFAULT NULL,
-  `html` text DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `url` (`url`)
-) ENGINE = MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-CREATE INDEX crawl_tag ON urls (crawl_tag);
+INSERT INTO `users` (id, username, pass) values (
+  1,
+  'root',
+  password('notyouraveragedefaultpassword')
+)
